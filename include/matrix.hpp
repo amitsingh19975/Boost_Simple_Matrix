6b0a6540236293d4c8e2a58faf16e3ca15e151c7
Amit Singh <amitsingh19975@gmail.com>
2019-02-20 10:30:20 +0530

    update 1

--- include/matrix.hpp
+++ include/matrix.hpp
@@ -4,29 +4,105 @@
 #include <vector>
 #include <iostream>
 
+/**
+ * Simple Matrix
+ */
 namespace simple{
     template<typename T>
     class Matrix{
     private:
+    /**
+     * private member for storing data
+     */
         std::vector<T> m_data;
+    /**
+     * prevate member for storing rows
+     */
         size_t m_row;    
+    /**
+     * prevate member for storing cols
+     */
         size_t m_col;    
     public:
+    /**
+     * default constructor
+     */
         Matrix():m_row(0),m_col(0){}
+    /**
+     * Constructor
+     * @param row type of size_t
+     * @param col type of size_t
+     */
         Matrix(size_t row, size_t col):m_row(row),m_col(col),m_data(col * row, 0){}
+    
+    /**
+     * Constructor
+     * @param row type of size_t
+     * @param col type of size_t
+     * @param initial type of any type for storing default value
+     */
         Matrix(size_t row, size_t col, T initial):m_row(row),m_col(col),m_data(col * row, initial){}
+        
+    /**
+     * Constructor
+     * @param row type of size_t
+     * @param col type of size_t
+     * @param data type of std::vector of any type
+     */
         Matrix(size_t row, size_t col, std::vector<T>& data):m_row(row),m_col(col),m_data(data){}
+    
+    /**
+     * Constructor
+     * @param row type of size_t
+     * @param col type of size_t
+     * @param data type of std::vector of any type move semantics
+     */
         Matrix(size_t row, size_t col, std::vector<T>&& data):m_row(row),m_col(col),m_data(std::move(data)){}
-        Matrix(Matrix<T>& m):m_row(m.m_row),m_col(m.m_col),m_data(m.m_data){}
+    
+    /**
+     * Copy Constructor
+     * @param m type of Matrix
+     */    
+        Matrix(Matrix<T> const& m):m_row(m.m_row),m_col(m.m_col),m_data(m.m_data){}
+    /**
+     * Move Constructor
+     * @param m type of Matrix
+     */
         Matrix(Matrix<T>&& m):m_row(std::move(m.m_row)),m_col(std::move(m.m_col)),m_data(std::move(m.m_data)){}
 
+    /**
+     * Operator() Overloading
+     * @param i type of size_t
+     * @param j type of size_t
+     */
         T& operator()(size_t i, size_t j);
+    
+    /**
+     * Operator[] Overloading
+     * for accessing linearly
+     * @param i type of size_t
+     */
         T& operator[](size_t i);
-        
+    
+    /**
+     * @returns the number of rows
+     */
         size_t row() const noexcept;
+
+    /**
+     * @returns the number of cols
+     */    
         size_t col() const noexcept;
+    
+    /**
+     * @returns the number of size of vector
+     */
         size_t size() const noexcept;
 
+    /**
+     * Operator<< Overloading
+     * for showing matrix
+     */
         friend std::ostream &operator<<(std::ostream &os, Matrix const &m){
             os<<'['<<' '<<'\n';
             for(size_t i = 0; i < m.row(); i++){
@@ -43,19 +119,79 @@ namespace simple{
             return os;
         }
 
+    /**
+     * Operator== Overloading
+     * @return Matrix
+     */
         Matrix& operator=(Matrix const &rhs);
+    /**
+     * Operator== Overloading
+     * @return Matrix
+     */
         Matrix& operator=(Matrix &&rhs);
+    /**
+     * Operator+ Overloading
+     * for addtion of two matrices
+     * @return Matrix
+     */
         Matrix& operator+(Matrix const &rhs);
+    
+    /**
+     * Operator- Overloading
+     * for subtraction of two matrices
+     * @return Matrix
+     */
         Matrix& operator-(Matrix const &rhs);
+    
+    /**
+     * Operator* Overloading
+     * for multipication of two matrices
+     * @return Matrix
+     */
         Matrix operator*(Matrix const &rhs);
 
+    /**
+     * Operator+= Overloading
+     * for addtion of two matrices
+     * and assigns them
+     * @return Matrix
+     */
         Matrix& operator+=(Matrix const &rhs);
+    
+    /**
+     * Operator-= Overloading
+     * for subtraction of two matrices
+     * and assigns them
+     * @return Matrix
+     */
         Matrix& operator-=(Matrix const &rhs);
+    /**
+     * Operator*= Overloading
+     * for multipication of two matrices
+     * and assigns them
+     * @return Matrix
+     */
         Matrix operator*=(Matrix const &rhs);
-        
+    
+    /**
+     * Operator== Overloading
+     * for comparision of two matrices
+     * @return bool true if they are equal
+     * otherwise false
+     */
         bool operator==(Matrix const &rhs);
+    
+    /**
+     * Operator!= Overloading
+     * for comparision of two matrices
+     * @return bool true if they are not equal
+     * otherwise false
+     */
         bool operator!=(Matrix const &rhs);
 
+    /**
+     * Destructor
+     */
         ~Matrix(){}
     };
 
