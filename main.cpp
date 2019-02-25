79c63ece262c2df668e07f8e828034eaf87fbd9b
Amit Singh <amitsingh19975@gmail.com>
2019-02-25 15:08:05 +0530

    improved testing-3

--- main.cpp
+++ main.cpp
@@ -6,22 +6,22 @@ using namespace simple;
 
 int main(){
     int size = 1000;
-    // test::testIntAssign(size);
-    // test::testIntCompare(size);
-    // test::testIntAdd(size);
-    // test::testIntAddAssign(size);
-    // test::testIntSub(size);
-    // test::testIntSubAssign(size);
+    test::testIntAssign(size);
+    test::testIntCompare(size);
+    test::testIntAdd(size);
+    test::testIntAddAssign(size);
+    test::testIntSub(size);
+    test::testIntSubAssign(size);
     test::testIntMul(size);
-    // test::testIntMulAssign(size);
-    // test::testComplexAssign(size);
-    // test::testComplexCompare(size);
-    // test::testComplexAdd(size);
-    // test::testComplexSub(size);
-    // test::testComplexSubAssign(size);
-    // test::testComplexAddAssign(size);
+    test::testIntMulAssign(size);
+    test::testComplexAssign(size);
+    test::testComplexCompare(size);
+    test::testComplexAdd(size);
+    test::testComplexSub(size);
+    test::testComplexSubAssign(size);
+    test::testComplexAddAssign(size);
     test::testComplexMul(size);
-    // test::testComplexMulAssign(size);
+    test::testComplexMulAssign(size);
 
     return 0;
 }
